68fe8515ca79bc65a1299369c0d8b74a0361ad33
krachi9 <55257249+krachi9@users.noreply.github.com>
2020-06-12 14:19:36 +0200

    Update nmr.user.js

--- nmr.user.js
+++ nmr.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Neptun Message Reader
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world or read some messages!
+// @version      0.2
+// @description  Try to take over the world and read some messages!
 // @author       Krachi9
 // @match        https://frame.neptun.bme.hu/hallgatoi/main.aspx?ismenuclick=true&ctrl=inbox
 // @match        https://frame.neptun.bme.hu/hallgatoi/main.aspx
